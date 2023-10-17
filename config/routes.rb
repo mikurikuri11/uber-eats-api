f494bb8a52ec03e91121015bb3ba6d394eb09b75
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-10-17 22:04:31 +0900

    Add: user model for google login feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :restaurants do
-        resources :foods, only: %i[index show]
+        resources :foods, only: [:index, :show]
       end
-      resources :line_foods, only: %i[index create]
-      put 'line_foods/replace', to: 'line_foods#replace'
-      resources :orders, only: %i[create]
+      resources :line_foods, only: [:index, :create]
+      put 'line_foods/replace', to: 'line_foods#replace', as: 'replace_line_foods'
+      resources :orders, only: [:create]
     end
   end
+
+  post 'auth/:provider/callback', to: 'api/v1/users#create', as: 'auth_callback'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_092259) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_17_043840) do
   create_table "foods", force: :cascade do |t|
     t.integer "restaurant_id", null: false
     t.string "name", null: false
@@ -44,6 +44,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_092259) do
     t.string "name", null: false
     t.integer "fee", default: 0, null: false
     t.integer "time_required", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
